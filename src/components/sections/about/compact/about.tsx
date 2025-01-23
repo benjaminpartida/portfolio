89f7059b90035fa1904ea6d8d30355c4a0dce481
benjaminpartida <55998785+benjaminpartida@users.noreply.github.com>
2025-01-23 00:52:37 +0000

    testing

--- src/components/sections/about/compact/about.tsx
+++ src/components/sections/about/compact/about.tsx
@@ -16,9 +16,16 @@ function About() {
             </h2>
             <div className="space-y-4">
               <p className="max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-                elit nisl, euismod a nibh pretium, cursus tincidunt metus. Sed
-                finibus mi eget consectetur pellentesque. Nulla at neque eros.
+                He desarrollado aplicaciones con Flutter y tengo experiencia en
+                tecnologías web, combinando mi pasión por el diseño y el
+                desarrollo. Me interesa especialmente el punto donde ambos
+                convergen, creando soluciones personalizadas a partir de las
+                ideas de los clientes. Me adapto fácilmente a las necesidades
+                del proyecto y disfruto superar desafíos. Tengo mayor
+                experiencia con Flutter, React, HTML/CSS/JS, Python y C++, pero
+                soy flexible y siempre estoy dispuesto a aprender nuevas
+                tecnologías. Mi afinidad por las computadoras y el aprendizaje
+                constante me impulsa a mejorar y crecer profesionalmente.
               </p>
               <Button asChild>
                 <Link href="resume.pdf" target="_blank">
